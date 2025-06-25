80eebdfdab3f035f681ec942b87a9f7c36237c28
dhruvi-sukhadiya <dhruvisukhadiya197@gmail.com>
2025-06-26 01:02:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+require("./models/index");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+function startServer() {
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    app.get("/", (req, res) => {
+        res.send("OK")
+    })
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`);
+    })
+}
+
+startServer()
